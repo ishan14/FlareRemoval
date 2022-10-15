b2547601732cd3513b70d33d2902f6559fd8eaeb
ishan14 <ishan.0114@gmail.com>
2022-10-15 19:27:31 +0530

    training message

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
         if opt.load_model:
             train_model.load_model()
         
+        print("Model training started")
         train_model.train()
         train_model.save_model()
 
